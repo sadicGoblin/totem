1352828ed66c3ce8a0796505e697476db1120481
fgallardo@favric.cl <sadic@MacBook-Pro-de-Felipe.local>
2025-07-21 10:48:11 -0400

    modal with add products

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,59 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/products.model';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent {
+  @Input() product: Product | null = null;
+  @Input() isOpen = false;
+  @Input() initialQuantity = 0;
+  
+  localQuantity = 0;
+  
+  @Output() close = new EventEmitter<void>();
+  @Output() addToCart = new EventEmitter<{product: Product, quantity: number}>();
+  
+  ngOnChanges(): void {
+    if (this.isOpen) {
+      this.localQuantity = this.initialQuantity;
+    }
+  }
+  
+  closeModal(): void {
+    this.close.emit();
+  }
+  
+  increase(): void {
+    this.localQuantity++;
+  }
+  
+  decrease(): void {
+    if (this.localQuantity > 0) {
+      this.localQuantity--;
+    }
+  }
+  
+  addToCartClicked(): void {
+    if (this.product && this.localQuantity > 0) {
+      this.addToCart.emit({product: this.product, quantity: this.localQuantity});
+      this.closeModal();
+    }
+  }
+  
+  formatPrice(price: number): string {
+    return '$' + price.toLocaleString('es-CL');
+  }
+  
+  // Cerrar modal si se hace clic en el fondo (backdrop)
+  onBackdropClick(event: MouseEvent): void {
+    if ((event.target as HTMLElement).classList.contains('modal-backdrop')) {
+      this.closeModal();
+    }
+  }
+}
